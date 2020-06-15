a127fc2406b341c143e1b75f8936a980b984e08a
sporting <sportingapp@gmail.com>
2020-06-15 11:20:00 +0800

    加入專案檔案。

---
+++ TestDynamicGetDataProvider/GetDataProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.Common;
+using System.Reflection;
+
+namespace TestDynamicGetDataProvider
+{
+    public class GetDataProvider
+    {
+        public GetDataProvider()
+        {
+            Register();
+        }
+
+        public DbConnection CreateConnection()
+        {
+            try
+            {
+                var a = Assembly.LoadFrom("Oracle.ManagedDataAccess.dll");
+                DbProviderFactory factory = (DbProviderFactory)a.CreateInstance("Oracle.ManagedDataAccess.Client.OracleClientFactory", true);
+                //DbProviderFactory factory = DbProviderFactories.GetFactory("Oracle.ManagedDataAccess.Client");
+                DbConnection con = factory.CreateConnection();
+                //con.ConnectionString = "xxxxxxxxxxxxxxxxx";
+                //con.Open();
+                //con.Close();
+                return con;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+        }
+
+        private void Register()
+        {
+            try
+            {
+                string invariantName = "Oracle.ManagedDataAccess.Client";
+                string factoryTypeAssemblyQualifiedName = "Oracle.ManagedDataAccess.Client.OracleClientFactory,Oracle.ManagedDataAccess";
+                DbProviderFactories.RegisterFactory(invariantName, factoryTypeAssemblyQualifiedName);
+                //DbProviderFactories.RegisterFactory(invariantName, OracleClientFactory.Instance);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ TestDynamicGetDataProvider/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestDynamicGetDataProvider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetDataProvider dp = new GetDataProvider();
+            dp.CreateConnection();
+        }
+    }
+}
